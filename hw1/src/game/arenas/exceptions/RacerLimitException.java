6b8871a588045b60e837988da4d83f310df1f7bc
orelhen <119847850+orelhen@users.noreply.github.com>
2023-04-15 18:48:28 +0300

    finall

--- hw1/src/game/arenas/exceptions/RacerLimitException.java
+++ hw1/src/game/arenas/exceptions/RacerLimitException.java
@@ -1,6 +1,7 @@
 package game.arenas.exceptions;
 
 public class RacerLimitException extends Exception {
+    //Racer , Recer num
     public RacerLimitException(String msg,String msg2){super("Arena is full! (" + msg +" active racer exist). racer #" + msg2 +" was not added");}
-   // Arena is full! (4 active racers exist). racer #13 was not added
+
 }
--- hw1/src/game/arenas/exceptions/RacerTypeException.java
+++ hw1/src/game/arenas/exceptions/RacerTypeException.java
@@ -1,6 +1,7 @@
 package game.arenas.exceptions;
 
 public class  RacerTypeException extends Exception {
+    //Racer type , Arena type
     public RacerTypeException(String msg,String msg2){super("inviald racer of type ''" +msg +"'' for "+msg2);}
-    //public RacerTypeException(String msg,Throwable cause){super(msg,cause);}
+
 }
