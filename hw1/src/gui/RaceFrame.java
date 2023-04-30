2dc302034fcc586db0d4a0d472c011c81c97b27a
orelhen <119847850+orelhen@users.noreply.github.com>
2023-04-30 15:30:34 +0300

    gui steps

--- hw1/src/gui/RaceFrame.java
+++ hw1/src/gui/RaceFrame.java
@@ -23,21 +23,147 @@ public class RaceFrame  {
 
         //right side panel
         JPanel rightpanel = new JPanel();
-        rightpanel.setBounds(900,0,200,700);
+        rightpanel.setBounds(910,0,200,700);
         rightpanel.setVisible(true);
         rightpanel.setBackground(Color.lightGray);
+        rightpanel.setLayout(null);
+
         frame.add(rightpanel);
 
         //Choose Arena ComboBox
+        JLabel ChooseArenalable = new JLabel("Choose arena:");
+        rightpanel.add(ChooseArenalable);
+        ChooseArenalable.setLocation(15, 10);
+        ChooseArenalable.setSize(150, 15);
+
         String[] Arenanames =  {"AerialArena", "NavalArena", "LandArena"};
         JComboBox SelectArena = new JComboBox(Arenanames);
         rightpanel.add(SelectArena);
-        SelectArena.setBounds(10,100,100,50);
-        SelectArena.setVisible(true);
+        SelectArena.setLocation(10, 30);
+        SelectArena.setSize(150, 25);
 
 
+        // arena size
+        JLabel ArenaLenlable = new JLabel("Arena length:");
+        rightpanel.add(ArenaLenlable);
+        ArenaLenlable.setLocation(15, 60);
+        ArenaLenlable.setSize(150, 15);
+
+        JTextField ArenaLengthfield = new JTextField("1000");
+        rightpanel.add(ArenaLengthfield);
+        ArenaLengthfield.setLocation(10, 80);
+        ArenaLengthfield.setSize(150, 25);
+
+        //Max arena racers
+        JLabel MaxRaceerslable = new JLabel("Max racers number:");
+        rightpanel.add(MaxRaceerslable);
+        MaxRaceerslable.setLocation(15, 110);
+        MaxRaceerslable.setSize(150, 15);
+
+        JTextField MaxRaceersfield = new JTextField("8");
+        rightpanel.add(MaxRaceersfield);
+        MaxRaceersfield.setLocation(10, 130);
+        MaxRaceersfield.setSize(150, 25);
+
+        //build arena btn
+        JButton buildArenaBut = new JButton("Build arena");
+        rightpanel.add(buildArenaBut);
+        buildArenaBut.setLocation(10, 175);
+        buildArenaBut.setSize(150, 30);
+//        buildArenaBut.addActionListener(this);
+
+        //seperator line
+        JSeparator Seperator1 = new JSeparator(SwingConstants.HORIZONTAL);
+        rightpanel.add(Seperator1);
+        Seperator1.setLocation(0, 220);
+        Seperator1.setSize(200, 10);
 
 
+        //Choose Racer ComboBox
+        JLabel ChooseRacerlable = new JLabel("Choose Racer:");
+        rightpanel.add(ChooseRacerlable);
+        ChooseRacerlable.setLocation(15, 230);
+        ChooseRacerlable.setSize(150, 15);
+
+        String[] Racernames =  {"Airplane", "Helicopter", "Bicycle","Car", "Horse", "RowBoat","SpeedBoat"};
+        JComboBox SelectRacer = new JComboBox(Racernames);
+        rightpanel.add(SelectRacer);
+        SelectRacer.setLocation(10, 250);
+        SelectRacer.setSize(150, 25);
+
+
+        //Choose Color ComboBox
+        JLabel ChooseColorlable = new JLabel("Choose color:");
+        rightpanel.add(ChooseColorlable);
+        ChooseColorlable.setLocation(15, 290);
+        ChooseColorlable.setSize(150, 15);
+
+        String[] colors =  {"Black", "Red", "Green","Blue", "Yellow"};
+        JComboBox SelectColor = new JComboBox(colors);
+        rightpanel.add(SelectColor);
+        SelectColor.setLocation(10, 310);
+        SelectColor.setSize(150, 25);
+
+        //Racer name
+        JLabel Namelable = new JLabel("Racers name:");
+        rightpanel.add(Namelable);
+        Namelable.setLocation(15, 350);
+        Namelable.setSize(150, 15);
+
+        JTextField Namefield = new JTextField();
+        rightpanel.add(Namefield);
+        Namefield.setLocation(10, 370);
+        Namefield.setSize(150, 25);
+
+
+        //Max speed
+        JLabel Maxspeedlable = new JLabel("Max speed:");
+        rightpanel.add(Maxspeedlable);
+        Maxspeedlable.setLocation(15, 410);
+        Maxspeedlable.setSize(150, 15);
+
+        JTextField Speedfield = new JTextField();
+        rightpanel.add(Speedfield);
+        Speedfield.setLocation(10, 430);
+        Speedfield.setSize(150, 25);
+
+
+        //Acceleration
+        JLabel Accelerationlable = new JLabel("Acceleration:");
+        rightpanel.add(Accelerationlable);
+        Accelerationlable.setLocation(15, 470);
+        Accelerationlable.setSize(150, 15);
+
+        JTextField Accelerationfield = new JTextField();
+        rightpanel.add(Accelerationfield);
+        Accelerationfield.setLocation(10, 490);
+        Accelerationfield.setSize(150, 25);
+
+        //add Racer arena btn
+        JButton addRacerBut = new JButton("Add Racer");
+        rightpanel.add(addRacerBut);
+        addRacerBut.setLocation(10, 530);
+        addRacerBut.setSize(150, 30);
+//        addRacerBut.addActionListener(this);
+
+        //seperator line
+        JSeparator Seperator2 = new JSeparator(SwingConstants.HORIZONTAL);
+        rightpanel.add(Seperator2);
+        Seperator2.setLocation(0, 570);
+        Seperator2.setSize(200, 10);
+
+
+        //Start Race btn
+        JButton StartBut = new JButton("Start Race");
+        rightpanel.add(StartBut);
+        StartBut.setLocation(10, 585);
+        StartBut.setSize(150, 30);
+
+        //Show info btn
+        JButton infoBut = new JButton("Show Info");
+        rightpanel.add(infoBut);
+        infoBut.setLocation(10, 625);
+        infoBut.setSize(150, 30);
     }
 
 
